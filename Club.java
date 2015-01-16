bacd5c60d7aafcd922cbf57df086bf3f7dc57413
Sourdaci <sourdaci@gmail.com>
2015-01-16 18:36:18 +0100

    el objeto Club almacena socios y muestra cuantos tiene almacenados

--- Club.java
+++ Club.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 /**
 * Store details of club memberships.
 *
@@ -5,24 +6,29 @@
 * @version (a version number or a date)
 */
 public class Club{
-	// Define any necessary fields here ...
+	// Socios del club
+	private ArrayList<Membership> socios;
+	
 	/**
 	* Constructor for objects of class Club
 	*/
 	public Club(){
-		// Initialise any fields here ...
+		socios = new ArrayList<Membership>();
 	}
+	
 	/**
 	* Add a new member to the club's list of members.
 	* @param member The member object to be added.
 	*/
 	public void join(Membership member){
+	    socios.add(member);
 	}
+	
 	/**
 	* @return The number of members (Membership objects) in
 	* the club.
 	*/
 	public int numberOfMembers(){
-		return 0;
+		return socios.size();
 	}
 }
